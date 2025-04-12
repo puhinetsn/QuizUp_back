692f51ab09f8f715d92aa84f451159365ff3ad13
puhinetsn <puhinetsn82@gmail.com>
2025-04-12 19:13:00 +0300

    question fixes added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import { errorHandler } from "./middleware/error.middleware.js";
 import questionsRouter from "./routes/questions.js";
 import categoriesRouter from "./routes/categories.js";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:4200" }));
 
 app.use("/api/questions", questionsRouter);
 app.use("/api/categories", categoriesRouter);
--- routes/questions.js
+++ routes/questions.js
@@ -4,24 +4,21 @@ import Question from "../models/question.model.js";
 import Category from "../models/category.model.js";
 
 router.get("/:category", async (req, res, next) => {
-  const { number, difficulty, type } = req.query;
-  const { category } = req.params;
+  const { number, difficulty } = req.query;
+  const category = req.params.category;
+  const categoryData = await Category.findById(category).lean();
 
-  if (!number || !category || !difficulty || !type) {
+  if (!number || !category || !difficulty) {
     return res.status(400).json({ error: "All parameters are required" });
   }
 
   const filter = {};
 
-  if (category !== "any") filter.category = category;
+  if (categoryData.category !== "Any") filter.category = category;
   if (difficulty !== "any") filter.difficulty = difficulty;
-  if (type !== "any") filter.type = type;
 
   try {
-    const [quiz, categoryData] = await Promise.all([
-      Question.find(filter).limit(Number(number)).lean(),
-      Category.findById(filter.category).lean(),
-    ]);
+    const quiz = await Question.find(filter).limit(Number(number)).lean();
 
     return res.status(200).json({ ...categoryData, quiz });
   } catch (err) {
