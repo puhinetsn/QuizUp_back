b56f7b3d2a0d6c7cad896ac98a8cd3bd596c1f79
puhinetsn <puhinetsn82@gmail.com>
2025-04-14 22:48:59 +0300

    random question fetch fix

--- routes/questions.js
+++ routes/questions.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import Question from "../models/question.model.js";
 import Category from "../models/category.model.js";
+import { Types } from "mongoose";
 
 router.get("/:category", async (req, res, next) => {
   const { number, difficulty } = req.query;
@@ -14,11 +15,19 @@ router.get("/:category", async (req, res, next) => {
 
   const filter = {};
 
-  if (categoryData.category !== "Any") filter.category = category;
+  if (categoryData.category !== "Any")
+    filter.category = new Types.ObjectId(category);
   if (difficulty !== "any") filter.difficulty = difficulty;
 
   try {
-    const quiz = await Question.find(filter).limit(Number(number)).lean();
+    const quiz = await Question.aggregate([
+      {
+        $match: filter,
+      },
+      {
+        $sample: { size: Number(number) },
+      },
+    ]);
 
     return res.status(200).json({ ...categoryData, quiz });
   } catch (err) {
