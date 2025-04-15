f18b183fb35d3dd5163309b7302a99858f13baf6
puhinetsn <puhinetsn82@gmail.com>
2025-04-15 22:14:16 +0300

    env added

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ import { errorHandler } from "./middleware/error.middleware.js";
 import questionsRouter from "./routes/questions.js";
 import categoriesRouter from "./routes/categories.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({ origin: process.env.ORIGIN }));
 
 app.use("/api/questions", questionsRouter);
 app.use("/api/categories", categoriesRouter);
 
 app.use(errorHandler);
 
-mongoose.connect(
-  "mongodb+srv://puhinetsn82:-gt9Gx4R.wZKVXg@clusterpuhinets.hjgmsud.mongodb.net/quizUp?retryWrites=true&w=majority&appName=ClusterPuhinets"
-);
+mongoose.connect(process.env.DATABASE_URL);
 
 app.listen(3000, () => console.log("Server ready"));
